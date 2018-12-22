d8bf4fa08a85ca19d5b321622e645ce30bd59b92
Nathan Robinson <nrobinson2000@me.com>
2018-12-22 16:26:48 -0500

    INITIAL at 16:26 of 2018-12-22

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,11 +1,43 @@
 #include "Particle.h"
+#include "oled-wing-adafruit/oled-wing-adafruit.h"
+
+OledWingAdafruit display;
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+void startupTasks()
+{
+  Serial.begin(115200);
+}
+STARTUP(startupTasks());
+
 
 void setup() // Put setup code here to run once
 {
+  display.setup();
 
+  	display.clearDisplay();
+  	display.display();
 }
 
 void loop() // Put code here to loop forever
 {
+  display.loop();
 
+  	if (display.pressedA()) {
+  		display.clearDisplay();
+
+  		display.setTextSize(1);
+  		display.setTextColor(WHITE);
+  		display.setCursor(0,0);
+  		display.println("CHUNGUS");
+  		display.display();
+  	}
+
+  	if (display.pressedB()) {
+  	}
+
+  	if (display.pressedC()) {
+  	}
 }
